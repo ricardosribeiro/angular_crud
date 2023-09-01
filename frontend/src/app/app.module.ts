ce0f3cfd88d7c5e77765e24989d4ed610df9f948
ricadosribeiro <ricardo.sribeiro@outlook.com>
2023-09-01 18:40:25 -0300

    Add Service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,23 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Application Components
-import { HeaderComponent } from './components/templates/header/header.component';
-import { FooterComponent } from './components/templates/footer/footer.component';
-import { NavComponent } from './components/templates/nav/nav.component';
-import { HomeComponent } from './views/home/home.component';
-
 // Material modules
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RedDirective } from './directives/red.directive';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+// Application Components
+import { HeaderComponent } from './components/templates/header/header.component';
+import { FooterComponent } from './components/templates/footer/footer.component';
+import { NavComponent } from './components/templates/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -30,7 +28,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     NavComponent,
     RedDirective,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -39,8 +38,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
-    
+    MatCardModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
